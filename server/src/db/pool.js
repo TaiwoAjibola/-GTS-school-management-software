9c159aa4581312063e50b5e47f15f4adaa01376f
Taiwo Ajibola <ajibola.ta@gmail.com>
2026-05-11 23:16:49 +0100

    Fix: use dns.lookup callback to force IPv4 for Supabase connection

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -1,3 +1,4 @@
+import dns from 'node:dns'
 import pg from 'pg'
 import { env } from '../config/env.js'
 
@@ -17,10 +18,14 @@ const parseDbUrl = (url) => {
 
 const dbConfig = parseDbUrl(env.databaseUrl)
 
+const lookup = (hostname, options, callback) => {
+  dns.lookup(hostname, { family: 4 }, callback)
+}
+
 export const pool = new Pool({
   ...(dbConfig || { connectionString: env.databaseUrl }),
   ssl: env.nodeEnv === 'production' ? { rejectUnauthorized: false } : false,
-  family: 4,
+  lookup,
 })
 
 export const query = (text, params = []) => pool.query(text, params)
